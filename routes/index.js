992a0bb3f40c21fc096c05e710e860c75f546f22
Maulana Yusuf <maulana@paxel.co>
2022-09-16 10:32:26 +0700

    boilerplate express sequelize

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+const classroomController = require('../controllers').classroom;
+const studentController = require('../controllers').student;
+const lecturerController = require('../controllers').lecturer;
+const courseController = require('../controllers').course;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* Classroom Router */
+router.get('/api/classroom', classroomController.list);
+router.get('/api/classroom/:id', classroomController.getById);
+router.post('/api/classroom', classroomController.add);
+router.put('/api/classroom/:id', classroomController.update);
+router.delete('/api/classroom/:id', classroomController.delete);
+
+/* Student Router */
+router.get('/api/student', studentController.list);
+router.get('/api/student/:id', studentController.getById);
+router.post('/api/student', studentController.add);
+router.put('/api/student/:id', studentController.update);
+router.delete('/api/student/:id', studentController.delete);
+
+
+module.exports = router;
